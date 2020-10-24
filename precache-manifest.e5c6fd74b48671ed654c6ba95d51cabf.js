c2f12cd60fc79a51597a13bd2826ad3e14f616ff
z-web-developer <73073714+z-web-developer@users.noreply.github.com>
2020-10-25 01:40:18 +0330

    Updates

---
+++ precache-manifest.e5c6fd74b48671ed654c6ba95d51cabf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847fa6d7d8870613e4872879c3993e39",
+    "url": "/z-resume/index.html"
+  },
+  {
+    "revision": "7fa833f5f2e0349024a7",
+    "url": "/z-resume/static/css/main.c7340dce.chunk.css"
+  },
+  {
+    "revision": "cfee501e4b0f450be0eb",
+    "url": "/z-resume/static/js/2.bdf959eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/z-resume/static/js/2.bdf959eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa833f5f2e0349024a7",
+    "url": "/z-resume/static/js/main.904eba75.chunk.js"
+  },
+  {
+    "revision": "69a83ff64930d5618651",
+    "url": "/z-resume/static/js/runtime-main.bccf2fcf.js"
+  }
+]);
